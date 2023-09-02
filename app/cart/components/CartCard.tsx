4c287fa64ba475f2ce4b1bc20ca483a4a2d0d07f
CodeMode365 <codemode365@gmail.com>
2023-09-02 22:37:49 +0545

    features fix

--- app/cart/components/CartCard.tsx
+++ app/cart/components/CartCard.tsx
@@ -2,20 +2,20 @@ import { Card, CardContent } from '@/components/ui/card'
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { ProductType } from '@/types/product'
 import { AiOutlinePlusCircle, AiOutlineMinusCircle } from 'react-icons/ai'
 import { Button } from '@/components/ui/button'
+import { cartItem } from '@/Providers/Redux/Slices/Cart'
 
 interface iProps {
-    incrementQuantity: (data: ProductType) => void
-    decrementQuantity: (data: ProductType) => void
-    deleteProduct: (data: ProductType) => void
-    data: ProductType
+    incrementQuantity: (data: cartItem) => void
+    decrementQuantity: (data: cartItem) => void
+    deleteProduct: (data: cartItem) => void
+    data: cartItem
 }
 const CartCard: React.FC<iProps> = ({ data, incrementQuantity, decrementQuantity, deleteProduct }) => {
     return (
         <Card className='grid grid-cols-12'>
-            <CardContent className='col-span-7 my-auto'>
+            <CardContent className='col-span-12 sm:col-span-7 my-auto border-r'>
                 <div className=' relative pb-52 overflow-hidden cursor-pointer '>
                     <Image
                         className="absolute inset-0 h-full w-full object-contain hover:scale-105 transition-all"
@@ -23,13 +23,13 @@ const CartCard: React.FC<iProps> = ({ data, incrementQuantity, decrementQuantity
                         alt="" fill />
                 </div>
             </CardContent>
-            <CardContent className='col-span-5 my-5'>
+            <CardContent className='col-span-12 md:col-span-5 my-5'>
                 <h2 className='text-xl font-semibold'>
                     {data.title.slice(0, 40)}
                 </h2>
                 <h4 className='text-lg mt-2'>Price: <span className='text-yellow-600'>${data.price} * {data.quantity} = <span className='text-yellow-500'>$ {Math.floor(data.price * data.quantity)}</span></span>
                 </h4>
-                <div className='flex items-center justify-between my-2 flex-wrap'>
+                <div className='flex flex-col justify-between my-2 '>
                     <div className='flex items-center'>
                         <Button size={"icon"} className='bg-rose-300 text-rose-500 hover:bg-rose-500 hover:text-rose-200' onClick={() => { incrementQuantity(data) }}>
                             <AiOutlinePlusCircle size={26} />
@@ -39,11 +39,11 @@ const CartCard: React.FC<iProps> = ({ data, incrementQuantity, decrementQuantity
                             <AiOutlineMinusCircle size={26} />
                         </Button>
                     </div>
-                    <Button size={"sm"} variant={'destructive'} className='mt-2 lg:mt-0' onClick={() => { deleteProduct(data) }}>
+                    <Button size={"sm"} variant={'destructive'} className='mt-2' onClick={() => { deleteProduct(data) }}>
                         Delete Item
                     </Button>
                 </div>
-                <div className='w-full flex mt-8'>
+                <div className='w-full flex mt-2'>
                     <Button size={"sm"} className='flex-1 bg-sky-500 hover:bg-sky-600 p-0 text-center flex items-center'>
                         <Link href={`/products/${data.id}`} className='h-full w-full text-center flex items-center justify-center'>
                             View Details
