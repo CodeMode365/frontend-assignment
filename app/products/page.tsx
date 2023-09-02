d0518e9b2567ec520de6d276445f58a7034f9af9
CodeMode365 <codemode365@gmail.com>
2023-09-02 23:18:25 +0545

    serach implementation

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
 import useFetch from '@/hooks/useFetch'
 import SingleCard from './components/SingleCard'
 import { ProductType } from '@/types/product'
@@ -14,21 +14,52 @@ const Products = () => {
   const { data, isLoading, isError }: { data: any, isLoading: boolean, isError: boolean } = useFetch()
 
 
+  const [finalData, setFinalData] = useState<any>();
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = useCallback(() => {
+
+    if (data) {
+      const filteredData = finalData.data.filter((item: ProductType) =>
+        item.title.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+
+      if (searchQuery.length <= 0) {
+        setFinalData(data)
+      } else {
+        setFinalData({ data: filteredData });
+      }
+    }
+  }, [searchQuery, data]);
+
+
+  useEffect(() => {
+    setFinalData(data || { data: [] })
+  }, [data]);
+
   return (
     !isError ? (
       <div className='mt-32 px-14'>
         <h2 className='text-2xl font-bold text-rose-500 text-center my-4'>Products</h2>
         <div className="flex w-full items-center space-x-2 justify-end " >
-          <Input type="text" placeholder="Search" className='max-w-sm' />
-          <Button type="submit" className='bg-rose-600 hover:bg-rose-600'>Search</Button>
+          <Input
+            type="text"
+            placeholder="Search" className='max-w-sm'
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <Button type="submit" className='bg-rose-600 hover:bg-rose-600' onClick={handleSearch}>Search</Button>
         </div>
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-10 m-4'>
           {
-            data &&
-            data.data.map((item: ProductType) => {
+            finalData &&
+            finalData.data.map((item: ProductType) => {
               return <SingleCard {...item} key={item.id} />
             })
           }
+          {finalData && finalData.data.length <=0 && (
+            <h2 className='text-red-400 text-2xl font-medium min-w-[270px]'>No Items Matched</h2>
+          )}
           {isLoading && !data && (
             Array.from({ length: 10 }, (_, index) => { return index + 1; }).map((_, ind) => (
               <LoadingBuffer key={ind + _} />
