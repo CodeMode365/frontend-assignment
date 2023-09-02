391d18e68a04c308bef4d29588920b3ba3058468
CodeMode365 <codemode365@gmail.com>
2023-09-02 20:36:46 +0545

    loading icon

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { VscLoading } from "react-icons/vsc"
 
 const loading = () => {
   return (
-    <div className='mt-32 w-full h-full bg-red-600'>loading</div>
+    <div className='mt-32 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-full'>
+      <VscLoading size={56} className="text-red-500 animate-spin -tranlsate-y-[50px]"/>
+    </div>
   )
 }
 
