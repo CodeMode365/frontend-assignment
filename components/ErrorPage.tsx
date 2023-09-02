eee1baf9c1a33f71b62215e88e99b734670283d7
CodeMode365 <codemode365@gmail.com>
2023-09-02 22:38:40 +0545

    error content

---
+++ components/ErrorPage.tsx
@@ -0,0 +1,25 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import React from 'react'
+import { useRouter } from 'next/navigation'
+
+const ErrorPage = () => {
+  const router = useRouter()
+  return (
+    <div className='w-screen h-screen flex items-center justify-center mt-16 max-w-screen-xl max-h-screen-lg'>
+      <div className='h-20 -translate-y-full w-[350px] md:w-[450px]'>
+        <div className='text-center'>
+          <h2 className='text-3xl font-bold'>Something went wrong!</h2>
+          <p className='leading-6 my-2'>The page you are trying to navigate doesn&apos;t exist or something went wrong while trying to navigate. Trying reloading the page or go to Home page.</p>
+        </div>
+        <div className='mx-auto flex justify-center my-2'>
+          <Button variant={'destructive'} className='mx-2' onClick={() => router.push("/")}>Go Home</Button>
+          <Button variant={'outline'} className='mx-2' onClick={() => router.refresh()}>Reload</Button>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default ErrorPage
