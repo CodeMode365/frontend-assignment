85a25f7ecd382149222965e1a3786814191544d5
CodeMode365 <codemode365@gmail.com>
2023-09-02 22:37:40 +0545

    features added

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,10 +1,11 @@
 'use client'
-import React from 'react'
+
+import React, { useState, useEffect } from 'react'
 import CartCard from './components/CartCard'
+import { Card, CardContent, CardHeader, } from '@/components/ui/card'
 import { AppDispatch, RootState } from '@/Providers/Redux/store'
 import { useDispatch, useSelector } from 'react-redux'
-import { ProductType } from '@/types/product'
-import { dcrQuantity, incQuantity, removeItem } from '@/Providers/Redux/Slices/Cart'
+import { cartItem, dcrQuantity, incQuantity, removeItem } from '@/Providers/Redux/Slices/Cart'
 
 const Cart = () => {
 
@@ -12,38 +13,65 @@ const Cart = () => {
   const dispatch = useDispatch<AppDispatch>()
 
 
-  const incrementQuantity = (data: ProductType) => {
+  const incrementQuantity = (data: cartItem) => {
     dispatch(incQuantity(data))
   }
-  const decrementQuantity = (data: ProductType) => {
+  const decrementQuantity = (data: cartItem) => {
     dispatch(dcrQuantity(data))
   }
 
-  const deleteProduct = (data: ProductType) => {
+  const deleteProduct = (data: cartItem) => {
     dispatch(removeItem(data))
   }
 
-  return (
-    <main className='mt-32 w-full '>
-      <h2 className='text-center text-2xl my-6'>Cart Items</h2>
-      <div className='grid grid-cols-6 w-11/12 gap-4 mx-auto pb-8'>
-        <div className='bg-white/70 col-span-6 md:col-span-4'>
-          {cartItems.map((item: ProductType) => {
-            return (
-              <div className='my-2' key={item.id}>
-                <CartCard data={item} incrementQuantity={incrementQuantity} decrementQuantity={decrementQuantity} deleteProduct={deleteProduct} />
-              </div>)
-          })}
-        </div>
 
-        <div className='bg-red-500 md:col-span-2 col-span-6 '>
-          <div className='sticky top-32 h-auto bg-purple-500'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus corporis mollitia adipisci consequuntur at nisi eum nulla. Consequatur, labore optio, eius harum in nulla eligendi hic numquam iste ex voluptatibus deleniti eveniet earum beatae voluptatum eum doloremque at voluptate officia maiores culpa? Eos quisquam voluptatem voluptates explicabo quae ea iusto corrupti eius, eveniet quod itaque necessitatibus error, exercitationem nobis vero.
+  const [totalAmount, setTotalAmount] = useState<number>(0)
+
+  useEffect(() => {
+    function calcTotal() {
+      let finalAmount = 0
+      cartItems.forEach((item) => {
+        finalAmount += (item.quantity * item.price)
+      })
+      setTotalAmount(Number(finalAmount.toFixed(2)))
+
+    }
+    calcTotal()
+    return calcTotal()
+  }, [cartItems])
+
+  {
+    return (
+      <main className='mt-32 w-full '>
+        <h2 className='text-2xl font-semibold text-rose-500 text-center my-4'>Cart Items</h2>
+        <div className='grid grid-cols-6 w-11/12 gap-4 mx-auto pb-8'>
+          <div className='bg-white/70 col-span-6 md:col-span-4'>
+            {cartItems.map((item: cartItem) => {
+              return (
+                <div className='my-2' key={item.id}>
+                  <CartCard data={item} incrementQuantity={incrementQuantity} decrementQuantity={decrementQuantity} deleteProduct={deleteProduct} />
+                </div>)
+            })}
+          </div>
+
+          <div className=' md:col-span-2 col-span-6 '>
+            <div className='sticky top-32 h-auto'>
+              <Card>
+                <CardHeader>
+                  <h2 className='text-2xl font-bold border-b'>SubTotal</h2>
+                </CardHeader>
+                <CardContent className='my-4 b-t-1 text-lg'>
+                  <h2 className='text-semibold text-lg'>Total Items: <span className='text-black/80'>{cartItems.length}</span></h2>
+                  <h2 className='text-semibold text-lg'>Total Price: <span className='text-black/80 text-yellow-500'> ${totalAmount}</span></h2>
+                </CardContent>
+              </Card>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
-  )
+      </main>
+    )
+  }
+
 }
 
 export default Cart
