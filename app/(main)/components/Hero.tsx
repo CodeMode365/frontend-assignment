3efe5291369b235e61294dfd2eba67a344d7c6f8
CodeMode365 <codemode365@gmail.com>
2023-09-02 22:10:26 +0545

    content change

--- app/(main)/components/Hero.tsx
+++ app/(main)/components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Hero = () => {
@@ -7,14 +8,14 @@ const Hero = () => {
             backgroundImage:"url('/back.jpg')"
         }}>
             <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">We invest in the world&apos;s potential</h1>
-                <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Shop The Future</h1>
+                <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Explore the cutting-edge of online shopping with us. Discover the latest trends, innovations, and convenience like never before as you 'Shop the Future' of retail</p>
                 <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                    <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-rose-700 hover:bg-rose-800 focus:ring-4 focus:ring-rose-300 ">
-                        Get started
-                    </a>
-                    <a href="#" className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
-                        Learn more
+                    <Link href="/products" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-rose-700 hover:bg-rose-800 focus:ring-4 focus:ring-rose-300 ">
+                        Explore
+                    </Link>
+                    <a href="https://github.com/CodeMode365" className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                        Thank Creator
                     </a>
                 </div>
             </div>
