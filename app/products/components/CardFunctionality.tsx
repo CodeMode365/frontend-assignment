c5afdad80cc7d9d1651367cc6c4befddc19dd620
CodeMode365 <codemode365@gmail.com>
2023-09-02 22:38:13 +0545

    feature content

--- app/products/components/CardFunctionality.tsx
+++ app/products/components/CardFunctionality.tsx
@@ -3,9 +3,19 @@ import { Button } from '@/components/ui/button'
 import { CardFooter } from '@/components/ui/card'
 import { useToast, } from '@/components/ui/use-toast'
 import { ToastAction } from "@/components/ui/toast"
+import { useDispatch } from 'react-redux'
+import { AppDispatch } from '@/Providers/Redux/store'
+import { ProductType } from '@/types/product'
+import { addToCart } from '@/Providers/Redux/Slices/Cart'
 
-const CardFunctionality = ({ title }: { title: string }) => {
+
+const CardFunctionality = ({ data }: { data: any }) => {
     const { toast } = useToast()
+    const dispatch = useDispatch<AppDispatch>()
+
+    const addItemToCart = (data: ProductType) => {
+        dispatch(addToCart(data))
+    }
 
     return (
         <CardFooter>
@@ -14,8 +24,9 @@ const CardFunctionality = ({ title }: { title: string }) => {
                     title: "Item added to Cart",
                     description: "Check your cart",
                     action: <ToastAction altText="Try again">Ok</ToastAction>,
-                    duration:1000
+                    duration: 1000
                 })
+                addItemToCart(data)
             }}>Add To cart</Button>
         </CardFooter>
     )
